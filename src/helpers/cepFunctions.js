0484379f631c396b09cdfae62df09bf02864026f
Arthur Teixeira <arthursony1@hotmail.com>
2023-03-24 00:36:36 -0300

    correção requisito 11

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,19 +1,17 @@
 export const getAddress = async () => {
   const input = document.querySelector('.cep-input');
-  const maxValueLength = 8;
-  if (!input.value || input.value.length !== maxValueLength) return 'CEP não encontrado';
   const api1 = await fetch(`https://cep.awesomeapi.com.br/json/${input.value}`);
   const api2 = await fetch(`https://brasilapi.com.br/api/cep/v2/${input.value}`);
+  try {
+    const data = Promise.any([api2, api1])
+      .then((response) => response.json());
 
-  return Promise.any([api2, api1])
-    .then((response) => response.json())
-    .then((data) => {
-      const result = `${(data
-        .andress || data
-        .street)} - ${(data.district || data.neighborhood)} - ${data.city} - ${data
-        .state}`;
-      return result;
-    });
+    const result = `${(data.andress || data.street)} - ${(data.district || data
+      .neighborhood)} - ${data.city} - ${data.state}`;
+    return result;
+  } catch {
+    return 'CEP não encontrado';
+  }
 };
 
 export const searchCep = async () => {
