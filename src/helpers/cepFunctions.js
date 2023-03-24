7dec6c35c9d3c1780ba1a8afde9d95073cd0d701
Arthur Teixeira <arthursony1@hotmail.com>
2023-03-24 00:10:48 -0300

    requisito 11

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,7 +1,23 @@
-export const getAddress = () => {
-  // seu código aqui
+export const getAddress = async () => {
+  const input = document.querySelector('.cep-input');
+  const maxValueLength = 8;
+  if (!input.value || input.value.length !== maxValueLength) return 'CEP não encontrado';
+  const api1 = await fetch(`https://cep.awesomeapi.com.br/json/${input.value}`);
+  const api2 = await fetch(`https://brasilapi.com.br/api/cep/v2/${input.value}`);
+
+  return Promise.any([api2, api1])
+    .then((response) => response.json())
+    .then((data) => {
+      const result = `${(data
+        .andress || data
+        .street)} - ${(data.district || data.neighborhood)} - ${data.city} - ${data
+        .state}`;
+      return result;
+    });
 };
 
-export const searchCep = () => {
-  // seu código aqui
+export const searchCep = async () => {
+  const cartAddress = document.querySelector('.cart__address');
+  const address = await getAddress();
+  cartAddress.innerText = address;
 };
